545d4dcb127ea015ddc52f443495321f2e4d3d7d
ejeiidjd <118985304+ejeiidjd@users.noreply.github.com>
2022-12-04 06:56:30 +0700

    Add files via upload

---
+++ 1048749155859828746-obfuscated.lua
@@ -0,0 +1,13 @@
+Uncaught exception:
+[string "bundle:deps/require.lua"]:301: .../discordobfuscatorbot/obfuscated/1048749155859828746.lua:149: function arguments expected near '='
+stack traceback:
+	[C]: in function 'assert'
+	[string "bundle:deps/require.lua"]:301: in function 'require'
+	[string "bundle:/main.lua"]:128: in function <[string "bundle:/main.lua"]:20>
+stack traceback:
+	[C]: in function 'error'
+	[string "bundle:/deps/utils.lua"]:41: in function 'assertResume'
+	[string "bundle:/init.lua"]:52: in function <[string "bundle:/init.lua"]:47>
+	[C]: in function 'xpcall'
+	[string "bundle:/init.lua"]:47: in function 'fn'
+	[string "bundle:deps/require.lua"]:310: in function <[string "bundle:deps/require.lua"]:266>
